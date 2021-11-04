f8f1f913293917af2bb5ace5e7ac98a4aa3a80e9
Jackson Johnson <JacksonJohnson789@gmail.com>
2021-11-04 17:53:53 -0400

    - Updated FetchKnife.java 11/04/2021 - Added null safe checks to all methods and cached all return values.

--- src/main/java/scripts/nodes/woodcutting/FetchKnife.java
+++ src/main/java/scripts/nodes/woodcutting/FetchKnife.java
@@ -11,20 +11,29 @@ import scripts.api.Workable;
 import scripts.api.antiban.AntiBan;
 import scripts.dax_api.shared.helpers.BankHelper;
 
+/**
+ * Purpose of class: Fetch a knife from the player's bank
+ *
+ * Updated 11/04/2021 - Added null safe checks to all methods and cached all return values.
+ */
+
 public class FetchKnife extends Node {
 
     @Override
     public void execute(Task task) {
         debug("Sleeping " + Workable.sleep(Globals.getWaitTimes(), AntiBan.getHumanFatigue()));
+
         debug("Retrieving knife");
 
         if (Bank.openBank()) {
             if (Banking.isBankLoaded()) {
                 if (Inventory.isFull()) {
-                    Banking.depositAll();
+                    if (Banking.depositAll() > 0) {
+                        debug("Deposited inventory");
+                    }
                 }
                 if (Banking.withdraw(1, Workable.KNIFE)) {
-                    boolean result = Timing.waitCondition(Workable::inventoryContainsKnife, General.random(300, 400));
+                    boolean result = Timing.waitCondition(Workable::inventoryContainsKnife, General.random(1000, 3000));
                     if (result) {
                         debug("Knife withdrew complete");
                     }
